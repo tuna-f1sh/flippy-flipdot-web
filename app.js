d4aa5d3ab0149cc994900d2d2091b0f894a68b57
John Whittington <john@jbrengineering.co.uk>
2017-08-11 08:36:38 +0100

    Backend stuff and basic layout

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express')
 var app = express();
-var io = require('socket.io')(http);
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
+var FlipDot = require('flipdot-display');
+// var flippy = new FlipDot('/dev/tty.wchusbserial1420',5,7,56);
+const SerialPort = require('serialport/test');
+const MockBinding = SerialPort.Binding;
+
+MockBinding.createPort('/dev/ROBOT', { echo: true });
+var flippy = new FlipDot('/dev/ROBOT',5,7,56);
 
 app.use(express.static('public'))
 
@@ -9,6 +17,62 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000, function(){
+server.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+flippy.on("error", function(err) {
+  console.log(err);
+});
+
+flippy.once("open", function() {
+
+  io.on('connection', function(socket){
+    flippy.clear();
+    matrix = flippy.matrix();
+
+    socket.on('flip', function(row, col) {
+      matrix[row][col] = !matrix[row][col];
+      flippy.send(flippy.writeMatrix(matrix));
+    });
+
+    socket.on('write text', function(text) {
+      // data = flippy.writeText(text);
+      var lines = text.split('\n');
+      var frames = [];
+      lines.forEach(function(line,i) {
+        frames.push(flippy.writeText(line,undefined,undefined,undefined,false));
+      });
+
+      flippy.writeFrames(frames,1000);
+
+      flippy.send();
+    });
+
+    flippy.on('sent', function() {
+      var display = decode(flippy.packet.data);
+      io.emit('update', display);
+    });
+
+  });
+
+});
+
+function decode(data) {
+  var hex_data = [];
+  for (var i = 0; i < data.length; i += 2) {
+    hex_data.push( asciiToByte( [data[i], data[i+1]] ) );
+  }
+
+  return hex_data;
+}
+
+
+function asciiToByte(chars) {
+  var b1 = String.fromCharCode(chars[1]);
+  var b2 = String.fromCharCode(chars[0]);
+
+  b2 = b2.concat(b1);
+
+  return parseInt(b2,16);
+}
--- public/js/dots.js
+++ public/js/dots.js
@@ -1,53 +1,101 @@
-// $(document).ready(function(){
-  var dotOn = '#d2ff4d';
-  var dotOff = '#303030';
+var socket = io();
 
-  // Setup the parameters for our grid. These are the values you can change.
-  var dotMargin = 2;
-  var numRows = 7;
-  var numCols = 56;
+var dotOn = '#d2ff4d';
+var dotOff = '#303030';
 
-  // Setup (explained earlier)
-  var canvas = $('canvas.dots');
-  var context = canvas[0].getContext('2d');
-  var canvasWidth = canvas.width();
-  var canvasHeight = canvas.height(); // this one is new
-  canvas.attr({height: canvasHeight, width: canvasWidth});
+// Setup the parameters for our grid. These are the values you can change.
+var dotMargin = 0;
+var numRows = 7;
+var numCols = 56;
 
-  // Because we don't know which direction (x vs. y) is the limiting sizing
-  // factor, we'll calculate both first.
-  var dotWidth = ((canvasWidth - (2 * dotMargin)) / numCols) - dotMargin;
-  var dotHeight = ((canvasHeight - (2 * dotMargin)) / numRows) - dotMargin;
+// Setup (explained earlier)
+var canvas = $('canvas.dots');
 
-  // Now, we use the limiting dimension to set the diameter.
-  if( dotWidth > dotHeight ) {
-    var dotDiameter = dotHeight;
-    var xMargin = (canvasWidth - ((2 * dotMargin) + (numCols * dotDiameter))) / numCols;
-    var yMargin = dotMargin;
-  } else {
-    var dotDiameter = dotWidth;
-    var xMargin = dotMargin;
-    var yMargin = (canvasHeight - ((2 * dotMargin) + (numRows * dotDiameter))) / numRows;
+var context = canvas[0].getContext('2d');
+var canvasWidth = canvas.width();
+var canvasHeight = canvas.height(); // this one is new
+canvas.attr({height: canvasHeight, width: canvasWidth});
+
+// Because we don't know which direction (x vs. y) is the limiting sizing
+// factor, we'll calculate both first.
+var dotWidth = ((canvasWidth - (2 * dotMargin)) / numCols) - dotMargin;
+var dotHeight = ((canvasHeight - (2 * dotMargin)) / numRows) - dotMargin;
+
+// Now, we use the limiting dimension to set the diameter.
+if( dotWidth > dotHeight ) {
+  var dotDiameter = dotHeight;
+  var xMargin = (canvasWidth - ((2 * dotMargin) + (numCols * dotDiameter))) / numCols;
+  var yMargin = dotMargin;
+} else {
+  var dotDiameter = dotWidth;
+  var xMargin = dotMargin;
+  var yMargin = (canvasHeight - ((2 * dotMargin) + (numRows * dotDiameter))) / numRows;
+}
+
+// Radius is still half of the diameter, because ... math.
+var dotRadius = dotDiameter * 0.5;
+
+// Now, we have to iterate in both directions, so we need a loop within a loop.
+// This loop is a little more complicated because the margin in the direction
+// with more space is not going to be the value you set.
+for(var i = 0; i < numRows; i++) { // i is the row iterator
+  for(var j = 0; j < numCols; j++) { // j is the column iterator
+  drawDot(i, j, false);
   }
+}
 
-  // Radius is still half of the diameter, because ... math.
-  var dotRadius = dotDiameter * 0.5;
+canvas[0].addEventListener('mousedown', function(evt) {
+  var mousePos = getMousePos(canvas[0], evt);
+  pos = getDot(mousePos.x, mousePos.y, dotRadius);
+  
+  console.log(pos);
 
-  // Now, we have to iterate in both directions, so we need a loop within a loop.
-  // This loop is a little more complicated because the margin in the direction
-  // with more space is not going to be the value you set.
-  for(var i = 0; i < numRows; i++) { // i is the row iterator
-    for(var j = 0; j < numCols; j++) { // j is the column iterator
-    var x = (j * (dotDiameter + xMargin)) + dotMargin + (xMargin / 2) + dotRadius;
-    var y = (i * (dotDiameter + yMargin)) + dotMargin + (yMargin / 2) + dotRadius;
-    drawDot(x, y, dotRadius, dotOn);
-    }
-  }
-// });
+  // drawDot(pos.row,pos.col,true);
+  socket.emit('flip',pos.row,pos.col);
+}, false);
 
-function drawDot(x, y, radius, colour) {
+function drawDot(i, j, active) {
   context.beginPath();
-  context.arc(x, y, radius, 0, 2 * Math.PI, false);
+  var x = (j * (dotDiameter + xMargin)) + dotMargin + (xMargin / 2) + dotRadius;
+  var y = (i * (dotDiameter + yMargin)) + dotMargin + (yMargin / 2) + dotRadius;
+  var colour = (active ? dotOn : dotOff);
+  context.arc(x, y, dotRadius, 0, 2 * Math.PI, false);
   context.fillStyle = colour;
   context.fill();
 }
+
+function getDot(x, y, radius) {
+  return {
+    col : Math.abs(Math.round((x - radius - (xMargin / 2) - dotMargin) / (dotDiameter + xMargin))),
+    row : Math.abs(Math.round((y - radius - (yMargin / 2) - dotMargin) / (dotDiameter + yMargin)))
+  };
+}
+
+function getMousePos(canvas, evt) {
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: Math.round((evt.clientX-rect.left)/(rect.right-rect.left)*canvas.width),
+    y: Math.round((evt.clientY-rect.top)/(rect.bottom-rect.top)*canvas.height)
+  };
+}
+
+$('form').submit(function(){
+  socket.emit('write text', $('#write-text-msg').val());
+  return false;
+});
+
+function updateDisplay(data) {
+  console.log('update');
+  // walk columns of bytes
+  for (var j = 0; j < 56; j++) {
+    // walk bits in byte constructing hex value
+    for (var i = 0; i < 8; i++) {
+      var bit = (data[j] >> i) & 0x01;
+      drawDot(i,j,bit);
+    }
+  }
+}
+
+socket.on('update', function(data) {
+  updateDisplay(data);
+});
