bb629eeeab63daa00c4c629b9d64c79639ae4e0f
John Whittington <git@jbrengineering.co.uk>
2017-11-16 08:54:49 +0000

    Rough and ready auto-sizing of canvas

--- public/js/dots.js
+++ public/js/dots.js
@@ -1,16 +1,5 @@
 var socket = io();
 
-var dotOn = '#d2ff4d';
-var dotOff = '#303030';
-
-// Setup the parameters for our grid. These are the values you can change.
-var dotMargin = 0;
-var numRows = 7;
-var numCols = 56;
-var clock = false;
-var glitter = false;
-var twitter = false;
-
 // Setup (explained earlier)
 var canvas = $('canvas.dots');
 
@@ -19,68 +8,60 @@ var canvasWidth = canvas.width();
 var canvasHeight = canvas.height(); // this one is new
 canvas.attr({height: canvasHeight, width: canvasWidth});
 
-// Because we don't know which direction (x vs. y) is the limiting sizing
-// factor, we'll calculate both first.
-var dotWidth = ((canvasWidth - (2 * dotMargin)) / numCols) - dotMargin;
-var dotHeight = ((canvasHeight - (2 * dotMargin)) / numRows) - dotMargin;
+var dotOn = '#d2ff4d';
+var dotOff = '#303030';
+var dotMargin = 0;
+var numRows = 0
+var numCols = 0
 
-// Now, we use the limiting dimension to set the diameter.
-if( dotWidth > dotHeight ) {
-  var dotDiameter = dotHeight;
-  var xMargin = (canvasWidth - ((2 * dotMargin) + (numCols * dotDiameter))) / numCols;
-  var yMargin = dotMargin;
-} else {
-  var dotDiameter = dotWidth;
-  var xMargin = dotMargin;
-  var yMargin = (canvasHeight - ((2 * dotMargin) + (numRows * dotDiameter))) / numRows;
-}
+var clock = false;
+var glitter = false;
+var twitter = false;
 
-// Radius is still half of the diameter, because ... math.
-var dotRadius = dotDiameter * 0.5;
+// Wait until socket sends raster to paint flip-dot correct size
+socket.on('raster', function(size) {
+  // Canvas dots taken from http://cobwwweb.com/animated-dot-background-canvas
+  // Setup the parameters for our grid. These are the values you can change.
+  numRows = size[0];
+  numCols = size[1];
+    // Because we don't know which direction (x vs. y) is the limiting sizing
+    // factor, we'll calculate both first.
+    var dotWidth = ((canvasWidth - (2 * dotMargin)) / size[1]) - dotMargin;
+    var dotHeight = ((canvasHeight - (2 * dotMargin)) / size[0]) - dotMargin;
 
-// Now, we have to iterate in both directions, so we need a loop within a loop.
-// This loop is a little more complicated because the margin in the direction
-// with more space is not going to be the value you set.
-for(var i = 0; i < numRows; i++) { // i is the row iterator
-  for(var j = 0; j < numCols; j++) { // j is the column iterator
-  drawDot(i, j, false);
+    // Now, we use the limiting dimension to set the diameter.
+    if( dotWidth > dotHeight ) {
+      var dotDiameter = dotHeight;
+      var xMargin = (canvasWidth - ((2 * dotMargin) + (size[1] * dotDiameter))) / size[1];
+      var yMargin = dotMargin;
+    } else {
+      var dotDiameter = dotWidth;
+      var xMargin = dotMargin;
+      var yMargin = (canvasHeight - ((2 * dotMargin) + (size[0] * dotDiameter))) / size[0];
+    }
+
+    // Radius is still half of the diameter, because ... math.
+    var dotRadius = dotDiameter * 0.5;
+
+  // Now, we have to iterate in both directions, so we need a loop within a loop.
+  // This loop is a little more complicated because the margin in the direction
+  // with more space is not going to be the value you set.
+  for(var i = 0; i < numRows; i++) { // i is the row iterator
+    for(var j = 0; j < numCols; j++) { // j is the column iterator
+    drawDot(i, j, size, false);
+    }
   }
-}
 
-canvas[0].addEventListener('mousedown', function(evt) {
-  var mousePos = getMousePos(canvas[0], evt);
-  pos = getDot(mousePos.x, mousePos.y, dotRadius);
-  
-  console.log(pos);
+  canvas[0].addEventListener('mousedown', function(evt) {
+    var mousePos = getMousePos(canvas[0], evt);
+    pos = getDot(mousePos.x, mousePos.y, dotRadius, [dotMargin, dotDiameter, xMargin, yMargin]);
+    
+    console.log(pos);
 
-  // drawDot(pos.row,pos.col,true);
-  socket.emit('flip',pos.row,pos.col);
-}, false);
-
-function drawDot(i, j, active) {
-  context.beginPath();
-  var x = (j * (dotDiameter + xMargin)) + dotMargin + (xMargin / 2) + dotRadius;
-  var y = (i * (dotDiameter + yMargin)) + dotMargin + (yMargin / 2) + dotRadius;
-  var colour = (active ? dotOn : dotOff);
-  context.arc(x, y, dotRadius, 0, 2 * Math.PI, false);
-  context.fillStyle = colour;
-  context.fill();
-}
-
-function getDot(x, y, radius) {
-  return {
-    col : Math.abs(Math.round((x - radius - (xMargin / 2) - dotMargin) / (dotDiameter + xMargin))),
-    row : Math.abs(Math.round((y - radius - (yMargin / 2) - dotMargin) / (dotDiameter + yMargin)))
-  };
-}
-
-function getMousePos(canvas, evt) {
-  var rect = canvas.getBoundingClientRect();
-  return {
-    x: Math.round((evt.clientX-rect.left)/(rect.right-rect.left)*canvas.width),
-    y: Math.round((evt.clientY-rect.top)/(rect.bottom-rect.top)*canvas.height)
-  };
-}
+    // drawDot(pos.row,pos.col,true);
+    socket.emit('flip',pos.row,pos.col);
+  }, false);
+});
 
 $('form').submit(function(){
   var e = document.getElementById('write-text-font');
@@ -106,7 +87,7 @@ function updateDisplay(data) {
     // walk bits in byte constructing hex value
     for (var i = 0; i < 8; i++) {
       var bit = (data[j] >> i) & 0x01;
-      drawDot(i,j,bit);
+      drawDot(i,j,[numRows, numCols],bit);
     }
   }
 }
@@ -176,6 +157,51 @@ function twitterSet() {
     $('#twitter-btn').text('Twitter: Off')
   }
 }
+
+function drawDot(i, j, size, active) {
+    // Because we don't know which direction (x vs. y) is the limiting sizing
+    // factor, we'll calculate both first.
+    var dotWidth = ((canvasWidth - (2 * dotMargin)) / size[1]) - dotMargin;
+    var dotHeight = ((canvasHeight - (2 * dotMargin)) / size[0]) - dotMargin;
+
+    // Now, we use the limiting dimension to set the diameter.
+    if( dotWidth > dotHeight ) {
+      var dotDiameter = dotHeight;
+      var xMargin = (canvasWidth - ((2 * dotMargin) + (size[1] * dotDiameter))) / size[1];
+      var yMargin = dotMargin;
+    } else {
+      var dotDiameter = dotWidth;
+      var xMargin = dotMargin;
+      var yMargin = (canvasHeight - ((2 * dotMargin) + (size[0] * dotDiameter))) / size[0];
+    }
+
+    // Radius is still half of the diameter, because ... math.
+    var dotRadius = dotDiameter * 0.5;
+
+    context.beginPath();
+    var x = (j * (dotDiameter + xMargin)) + dotMargin + (xMargin / 2) + dotRadius;
+    var y = (i * (dotDiameter + yMargin)) + dotMargin + (yMargin / 2) + dotRadius;
+    var colour = (active ? dotOn : dotOff);
+    context.arc(x, y, dotRadius, 0, 2 * Math.PI, false);
+    context.fillStyle = colour;
+    context.fill();
+}
+
+function getDot(x, y, radius, [dotMargin, dotDiameter, xMargin, yMargin]) {
+  return {
+    col : Math.abs(Math.round((x - radius - (xMargin / 2) - dotMargin) / (dotDiameter + xMargin))),
+    row : Math.abs(Math.round((y - radius - (yMargin / 2) - dotMargin) / (dotDiameter + yMargin)))
+  };
+}
+
+function getMousePos(canvas, evt) {
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: Math.round((evt.clientX-rect.left)/(rect.right-rect.left)*canvas.width),
+    y: Math.round((evt.clientY-rect.top)/(rect.bottom-rect.top)*canvas.height)
+  };
+}
+
 
 // sent page loaded event
 socket.emit('loaded');
